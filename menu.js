d783ed787eb281dccf22f07fd73ff9d8094cf91b
tsohleDev <mokhemisitsohle@gmail.com>
2022-11-25 14:34:58 +0200

    Fix linters

--- menu.js
+++ menu.js
@@ -1,14 +1,14 @@
 const hamburger = document.querySelector('#hamburger');
-const cross = document.querySelector('#cross')
-const headerTag = document.querySelector('header')
+const cross = document.querySelector('#cross');
+const headerTag = document.querySelector('header');
 
 hamburger.addEventListener('click', () => {
-    headerTag.style.display = 'flex'
-    headerTag.style.flexDirection = 'column'
-    headerTag.style.width = '100%'
-    headerTag.style.height = '100%'
-})
+  headerTag.style.display = 'flex';
+  headerTag.style.flexDirection = 'column';
+  headerTag.style.width = '100%';
+  headerTag.style.height = '100%';
+});
 
 cross.addEventListener('click', () => {
-    headerTag.style.display = 'none'
-})
+  headerTag.style.display = 'none';
+});
